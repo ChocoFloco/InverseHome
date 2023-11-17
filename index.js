274b3214afd6a5407ecc8d2ddcc5399d55a610b2
ChocoFloco <149216961+ChocoFloco@users.noreply.github.com>
2023-11-16 20:50:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,6 @@ import createBareServer from "@tomphttp/bare-server-node";
 import path from 'node:path';
 import cors from 'cors';
 
-const express = require('express');
-const app = express()
-const dir = __dirname + "/static/"
-
-app.get("/", (req, res) => {
-res.sendFile(dir + "index.html")
-})
-
-app.get("/p", (req, res) => {
-if(req.query.password === process.env.PASSWORD) {
-res.sendFile(dir + "home.html")
-} else {
-  res.sendFile(dir + "index.html")
-}
-})
-
-app.listen(process.env.PORT)
-
 const __dirname = process.cwd();
 const server = http.createServer();
 const app = express(server);
